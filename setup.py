7195144cc0b43edead56b88e9592444541f4d4b7
Swati <swatimishra0209@gmail.com>
2025-01-05 00:04:03 +0530

    adding setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # Strip newlines and spaces
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='MLProject-Template',
+    version='0.0.1',
+    author='Swati',
+    author_email='swatimishra0209@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")  # Use the function to get requirements
+)
