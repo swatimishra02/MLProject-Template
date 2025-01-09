bc1f7e9fe3b1a117156bd9447bb7d2dba0e95f32
Swati <swatimishra0209@gmail.com>
2025-01-09 19:07:41 +0530

    model deployment

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,9 +11,9 @@ from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 # these are the inputs that I am giving to my data ingestion component
 @dataclass
 class DataIngestionConfig :
-    train_data_path: str=os.path.join('artifact', 'train.csv')
-    test_data_path: str=os.path.join('artifact', 'test.csv')
-    raw_data_path: str=os.path.join('artifact', 'data.csv')
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
+    test_data_path: str=os.path.join('artifacts', 'test.csv')
+    raw_data_path: str=os.path.join('artifacts', 'data.csv')
 
 
 class DataIngestion :
